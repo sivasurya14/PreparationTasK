480df512189a04ed9c070457f534a57b5b8ab60a
sivasuriyan <sivasuriyan0120@gmail.com>
2023-07-08 11:02:44 +0530

    changes commit in new changes

--- src/main/java/com/myapp/vehicle/CustomerController.java
+++ src/main/java/com/myapp/vehicle/CustomerController.java
@@ -5,15 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
-	
+
 	@GetMapping("/getMessage")
-	public String getMessage() {		
+	public String getMessage() {
 		return " welcome to Customer details page ";
 	}
-	
+
 	@GetMapping("/getNewMessage")
 	public String getNewMessage() {
 		return "this is new changes";
+	}
+
+	@GetMapping("/getNewMessage2")
+
+	public String restNewBranch() {
+		return "the branch is created ";
+
 	}
 
 }
